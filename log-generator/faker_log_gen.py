3497e846273cd2164bf3aeeeefbc9ef6005265e2
0BVer <dr0joon@gmail.com>
2023-06-15 21:44:38 +0900

    feat : changing log generating plan

--- log-generator/faker_log_gen.py
+++ log-generator/faker_log_gen.py
@@ -1,5 +1,4 @@
 import json
-import time
 import datetime
 import numpy
 import random
@@ -12,15 +11,14 @@ method = ['GET', 'POST', 'PUT', 'DELETE']
 response = ["200", "201", "403", "404", "500", "301"]
 
 
-def create_log(log_per_sec: int, server_name: str, resources: list, producer: KafkaProducer) -> None:
-    time_now = datetime.datetime.now(tz=tzlocal.get_localzone())
-    # tz = datetime.datetime.now().strftime('%z')
-    dt = time_now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+def create_log(server_name: str, resources: list, producer: KafkaProducer) -> None:
 
-    # f = open(server_name + '_access_log_' + time.strftime("%Y%m%d") + '.log', 'w')
+    while True:
+        time_now = datetime.datetime.now(tz=tzlocal.get_localzone())
+        dt = time_now.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
-    start_time = time.time()
-    for _ in range(log_per_sec):
+        # f = open(server_name + '_access_log_' + time.strftime("%Y%m%d") + '.log', 'w')
+
         ip = faker.ipv4()
         vrb = numpy.random.choice(method, p=[0.7, 0.1, 0.1, 0.1])
         uri = random.choice(resources)
@@ -36,6 +34,3 @@ def create_log(log_per_sec: int, server_name: str, resources: list, producer: Ka
         producer.send(topic=server_name, value=json_tf.encode(encoding='utf-8'))
         producer.flush()
 
-    end_time = time.time()
-    execution_time = (end_time - start_time) * 1000  # 초를 밀리초로 변환
-    print(f"{server_name} 생성 실행 시간: {execution_time:.2f} ms")
--- log-generator/log-gen.py
+++ log-generator/log-gen.py
@@ -1,5 +1,4 @@
 import threading
-import time
 
 from kafka import KafkaProducer
 from kafka_setup import kafka_topic_setup
@@ -8,29 +7,24 @@ from log_resource import user_resources, post_resources, mail_resources
 
 bootstrap_server = ["127.0.0.1:19092", "127.0.0.1:29092", "127.0.0.1:39092"]
 prod = KafkaProducer(bootstrap_servers=bootstrap_server)
-log_per_sec = 3
 
 
 def create_user_server_log() -> None:
-    create_log(log_per_sec=log_per_sec, server_name='user', resources=user_resources, producer=prod)
+    create_log(server_name='user', resources=user_resources, producer=prod)
 
 
 def create_post_server_log() -> None:
-    create_log(log_per_sec=log_per_sec, server_name='post', resources=post_resources, producer=prod)
+    create_log(server_name='post', resources=post_resources, producer=prod)
 
 
 def create_mail_server_log() -> None:
-    create_log(log_per_sec=log_per_sec, server_name='mail', resources=mail_resources, producer=prod)
+    create_log(server_name='mail', resources=mail_resources, producer=prod)
 
 
 if __name__ == '__main__':
     kafka_topic_setup()
 
     print("[start generating]")
-    for _ in range(600):
-        threading.Thread(target=create_user_server_log).start()
-        threading.Thread(target=create_post_server_log).start()
-        threading.Thread(target=create_mail_server_log).start()
-        time.sleep(1)
-
-    print("[end generating]")
+    threading.Thread(target=create_user_server_log).start()
+    threading.Thread(target=create_post_server_log).start()
+    threading.Thread(target=create_mail_server_log).start()
